032a186f63b3a005f6f7d4b9d02b5cc6826252db
kali-vm-main <foo@bar.baz>
2022-06-02 12:12:20 +0200

    Fixed order of var defs

--- lkd_run.sh
+++ lkd_run.sh
@@ -15,11 +15,11 @@ export COMMIT=e783362eb54cd99b2cac8b3a9aeac942e6f6ac07
 export LOGGING_ON=1
 
 # Variables you may not want to change
-export PATH_SSHD_CONF=$(pwd)/$FILES/lkd_sshd_config
 export IMG=lkd_qemu_image.qcow2
 export DIR=mount-point.dir
 export FILES=lkd_files
 export SCRIPTS=lkd_scripts_sh
+export PATH_SSHD_CONF=$(pwd)/$FILES/lkd_sshd_config
 
 source $SCRIPTS/lkd_functions.sh
 
