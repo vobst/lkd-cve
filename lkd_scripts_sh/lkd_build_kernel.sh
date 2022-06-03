67c70f776f40a197d68c78cd96c47adf2acd3c0c
kali-vm-main <foo@bar.baz>
2022-06-03 22:43:26 +0200

    option to use per project .config

--- lkd_scripts_sh/lkd_build_kernel.sh
+++ lkd_scripts_sh/lkd_build_kernel.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 
-make mrproper && \
-make x86_64_defconfig && \
-make kvm_guest.config && \
-./scripts/config \
-    -e CONFIG_USER_NS \
-    -m CONFIG_NF_TABLES \
-    -e DEBUG_KERNEL \
-    -e DEBUG_INFO \
-    -e DEBUG_INFO_DWARF4 \
-    -e FRAME_POINTER \
-    -e GDB_SCRIPTS \
-    -e KALLSYMS \
-    -d DEBUG_INFO_BTF \
-    -d DEBUG_INFO_DWARF5 \
-    -d DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT \
-    -d DEBUG_INFO_REDUCED \
-    -d DEBUG_INFO_COMPRESSED \
-    -d DEBUG_INFO_SPLIT \
-    -d RANDOMIZE_BASE && \
+source $SCRIPTS/lkd_functions.sh
+
+make mrproper || exit 1
+
+if [[ -f $EXAMPLES/$PROJECT/.config ]]
+then
+  log "Using costum .config for $PROJECT"
+  cp $EXAMPLES/$PROJECT/.config .
+else
+  log "Using generic .config"
+  make x86_64_defconfig && \
+  ./scripts/config \
+      -e DEBUG_KERNEL \
+      -e DEBUG_INFO \
+      -e DEBUG_INFO_DWARF4 \
+      -e FRAME_POINTER \
+      -e GDB_SCRIPTS \
+      -e KALLSYMS \
+      -d DEBUG_INFO_BTF \
+      -d DEBUG_INFO_DWARF5 \
+      -d DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT \
+      -d DEBUG_INFO_REDUCED \
+      -d DEBUG_INFO_COMPRESSED \
+      -d DEBUG_INFO_SPLIT \
+      -d RANDOMIZE_BASE || exit 1
+fi
+
 make -j$(nproc) all && \
-make -j$(nproc) modules 
+make -j$(nproc) modules || exit 1
+
+exit 0
