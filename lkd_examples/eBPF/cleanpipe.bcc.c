89b9d8748837f230dd8e6ffd78fc2279d582fcd9
kali-vm-main <foo@bar.baz>
2022-06-23 19:26:48 +0200

    Save

--- lkd_examples/eBPF/cleanpipe.bcc.c
+++ lkd_examples/eBPF/cleanpipe.bcc.c
@@ -6,6 +6,7 @@ extern const struct file_operations pipefifo_fops;
 struct data_t {
   u32 pid;
   u64 ts;
+  u8 ok;
   char comm[TASK_COMM_LEN];
 };
 
@@ -14,8 +15,14 @@ BPF_RINGBUF_OUTPUT(buffer, 8);
 LSM_PROBE(file_permission, struct file *file, int mask)
 {
   struct data_t data = {};
+  struct pipe_inode_info *pipe;
+  struct pipe_buffer *buf;
+  struct pipe_buf_operations *ops;
+  u64 flags;
+  u32 pipe_mask, head, ring_size;
 
-  if (file->f_op != (struct file_operations *)PIPEFIFO_FOPS) 
+  if (file->f_op != (struct file_operations *)PIPEFIFO_FOPS
+		  || mask == MAY_READ) 
   {
     return 0;
   }
@@ -23,9 +30,26 @@ LSM_PROBE(file_permission, struct file *file, int mask)
   data.pid = bpf_get_current_pid_tgid();
   data.ts = bpf_ktime_get_ns();
   bpf_get_current_comm(&data.comm, sizeof(data.comm));
+  data.ok = 0;
+
+  pipe = (struct pipe_inode_info *)file->private_data;
+  bpf_probe_read_kernel(&ring_size, sizeof(u32), &pipe->ring_size);
+  bpf_probe_read_kernel(&head, sizeof(u32), &pipe->head);
+  pipe_mask = ring_size - 1;
+
+  bpf_probe_read_kernel(&buf, sizeof(u64), &pipe->bufs);
+  buf = buf + (head & pipe_mask);
+
+  bpf_probe_read_kernel(&flags, sizeof(u64), &buf->flags);
+  bpf_probe_read_kernel(&ops, sizeof(u64), &buf->ops);
+
+  if (flags & PIPE_BUF_FLAG_CAN_MERGE && ops == (struct pipe_buf_operations *)PAGE_CACHE_PIPE_BUF_OPS)
+  { 
+    data.ok = 1;
+  }
 
   buffer.ringbuf_output(&data, sizeof(data), 0);
 
-  return 0;
+  return data.ok;
 }
 
--- lkd_examples/eBPF/cleanpipe.bcc.py
+++ lkd_examples/eBPF/cleanpipe.bcc.py
@@ -4,9 +4,12 @@ import time
 import sys
 from bcc import BPF
 
+b = None
+
 def callback(ctx, data, size):
+    global b
     event = b['buffer'].event(data)
-    print("%-16s %10d %10d" % (event.comm.decode('utf-8'), event.pid, event.ts))
+    print("%-16s %10d %10d %10d" % (event.comm.decode('utf-8'), event.pid, event.ts % 100, event.ok))
 
 def lookup_sym(sym):
     with open("/proc/kallsyms") as syms:
@@ -14,7 +17,7 @@ def lookup_sym(sym):
         for line in syms:
             (addr, size, name) = line.rstrip().split(" ", 2)
             name = name.split("\t")[0]
-            if name == "pipefifo_fops":
+            if name == sym:
                 sym_p = "0x" + addr
                 break
         if sym_p == '':
@@ -22,20 +25,29 @@ def lookup_sym(sym):
             exit(1)
         return sym_p
 
-with open("cleanpipe.bcc.c", "r") as f:
-    text = f.read()
-    text = text.replace("PIPEFIFO_FOPS", lookup_sym("pipefifo_fops"))
+def get_text():
+    with open("cleanpipe.bcc.c", "r") as f:
+        text = f.read()
+        text = text.replace("PIPEFIFO_FOPS", lookup_sym("pipefifo_fops"))
+        text = text.replace("PAGE_CACHE_PIPE_BUF_OPS", lookup_sym("page_cache_pipe_buf_ops"))
+    return text
 
-b = BPF(text=text)
-b['buffer'].open_ring_buffer(callback)
+def main():
+    global b
+    b = BPF(text=get_text())
+    b['buffer'].open_ring_buffer(callback)
 
-print("LSM hook file_permission on pipe operations, ctrl-c to exit.")
+    print("LSM hook file_permission on pipe write attempts, ctrl-C to exit.")
+    print("%-16s %10s %10s %10s" % ("COMM", "PID", "TIME", "EXPLOIT???"))
 
-print("%-16s %10s %10s %10s" % ("COMM", "PID", "TIME", "PIPE"))
+    try:
+        while 1:
+            b.ring_buffer_poll()
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        return 0
+    
+    return 1
 
-try:
-    while 1:
-        b.ring_buffer_poll()
-        time.sleep(0.5)
-except KeyboardInterrupt:
-    sys.exit(0)
+if __name__ == "__main__":
+    sys.exit(main())
